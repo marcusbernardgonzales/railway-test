c46c5cc2cbd110c893f5123521a91ffee57e3618
jgfiguerres-at <justin.edward.figuerres@student.ateneo.edu>
2026-04-26 21:36:42 +0800

    Update item_update.html and urls.py

--- merchstore/urls.py
+++ merchstore/urls.py
@@ -3,9 +3,18 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('merchstore/items', ItemListView.as_view(), name='item_list'),
+    path('merchstore/items', ItemListView.as_view(),
+         name='item_list'),
     path('merchstore/item/<int:pk>', ItemDetailView.as_view(),
          name='item_detail'),
+    path('merchstore/item/<int:pk>/edit', ItemUpdateView.as_view(),
+         name='item_update'),
+    path('merchstore/item/add', ItemCreateView.as_view(),
+         name='item_create'),
+    path('merchstore/cart', CartView.as_view(),
+         name='cart'),
+    path('merchstore/transactions', TransactionDetailView.as_view(),
+         name='transaction_detail'),
 ]
 
 app_name = "merchstore"
