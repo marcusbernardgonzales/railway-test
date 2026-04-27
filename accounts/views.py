b92939c3be559dbee148d6977ddf1ec00dcc5571
Marcus Gonzales <marcus.bernard.gonzales@student.ateneo.edu>
2026-04-27 15:23:23 +0800

    Implement ProfileUpdateView

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import UpdateView
 
-# Create your views here.
+from .models import Profile
+
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    model = Profile
+    fields = ['display_name']
+    template_name = 'accounts/profile_update.html'
+
+    def get_object(self):
+        return self.request.user.profile
+
+    def get_success_url(self):
+        return '/'
+
