3c7e617483e9551a05afbecc0851cc81f9c30327
Marcus Gonzales <marcus.bernard.gonzales@student.ateneo.edu>
2026-05-01 18:42:19 +0800

    Implement EventList view

--- localevents/views.py
+++ localevents/views.py
@@ -9,6 +9,27 @@ class EventListView(ListView):
     template_name = 'localevents/event_list.html'
     context_object_name = 'events'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if self.request.user.is_authenticated:
+            profile = self.request.user.profile
+
+            created = Event.objects.filter(organizer=profile)
+            signed = Event.objects.filter(eventsignup__user_registrant=profile)
+
+            other = Event.objects.exclude(organizer=profile)
+            other = other.exclude(eventsignup__user_registrant=profile)
+
+            context['created_events'] = created
+            context['signed_events'] = signed
+            context['all_events'] = other
+
+        else:
+            context['all_events'] = Event.objects.all()
+
+        return context
+
 
 class EventDetailView(DetailView):
     model = Event
