6ca3dbc4f53d2c0584b76b5320a3800ea8dcde35
jacob-guda06 <jacob.guda@student.ateneo.edu>
2026-05-04 20:59:22 +0800

    Edit ProjectListView()

--- diyprojects/views.py
+++ diyprojects/views.py
@@ -9,6 +9,17 @@ class ProjectListView(ListView):
     model = Project
     template_name = 'diyprojects/project_list.html' # default value
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        profile = self.request.user.profile
+
+        context['created'] = Project.objects.filter(creator=profile)
+        context['favorited'] = Project.objects.filter(favorites__profile=profile)
+        context['reviewed'] = Project.objects.filter(reviews__reviewer=profile)
+
+        return context
+
+
 
 class ProjectDetailView(DetailView):
     model = Project
