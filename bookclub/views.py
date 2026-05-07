595f3ebf4fb775fc3b317f6805eae23184351488
Coby Velasco <carlos.velasco@student.ateneo.edu>
2026-05-07 12:54:49 +0800

    Update views to have similar format to other apps

--- bookclub/views.py
+++ bookclub/views.py
@@ -1,13 +1,25 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, View
+from datetime import timedelta
+
+from django.views import View
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import Group
 from django.shortcuts import get_object_or_404, redirect, render
 
-from .models import Book, BookReview, Bookmark
+from .models import Book, BookReview, Bookmark, Borrow
 from .forms import BookForm, BookReviewForm, BorrowForm
 
 
 def is_book_contributor(user):
-    return user.groups.filter(name='Book Contributors').exists()
+    return ( 
+        user.is_authenticated and 
+        hasattr(user, "profile") and 
+        (
+        user.groups.filter(name="Commission Maker").exists() 
+        or user.is_superuser 
+        )
+    )
+
 
 
 class BookListView(ListView):
@@ -15,35 +27,33 @@ class BookListView(ListView):
     template_name = 'bookclub/book_list.html'
     context_object_name = 'books'
 
-    def get_queryset(self):
-        qs = Book.objects.all()
-
-        if self.request.user.is_authenticated and hasattr(self.request.user, 'profile'):
-            profile = self.request.user.profile
-
-            contributed = qs.filter(contributer=profile)
-            bookmarked = qs.filter(bookmark__profile=profile)
-            reviewed = qs.filter(bookreview__user_reviewer=profile)
-
-            excluded_ids = (
-                contributed.values_list('id', flat=True) |
-                bookmarked.values_list('id', flat=True) |
-                reviewed.values_list('id', flat=True)
-            )
-
-            qs = qs.exclude(id__in=excluded_ids)
-
-        return qs
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
         if self.request.user.is_authenticated and hasattr(self.request.user, 'profile'):
             profile = self.request.user.profile
 
-            context['contributed_books'] = Book.objects.filter(contributer=profile)
-            context['bookmarked_books'] = Book.objects.filter(bookmark__profile=profile)
-            context['reviewed_books'] = Book.objects.filter(bookreview__user_reviewer=profile)
+            contributed = Book.objects.filter(contributor=profile)
+
+            bookmarked = Book.objects.filter(
+                bookmark__profile=profile
+            )
+
+            reviewed = Book.objects.filter(
+                bookreview__user_reviewer=profile
+            )
+
+            other = Book.objects.exclude(contributor=profile)
+            other = other.exclude(bookmark__profile=profile)
+            other = other.exclude(bookreview__user_reviewer=profile)
+
+            context['contributed_books'] = contributed
+            context['bookmarked_books'] = bookmarked
+            context['reviewed_books'] = reviewed
+            context['all_books'] = other
+
+        else:
+            context['all_books'] = Book.objects.all()
 
         return context
 
@@ -62,42 +72,78 @@ class BookDetailView(DetailView):
         context['review_form'] = BookReviewForm()
         context['reviews'] = book.bookreview_set.all()
         context['bookmark_count'] = book.bookmark_set.count()
-        context['available'] = book.available_to_borrow
+        
+        is_owner = False
 
-        if user.is_authenticated and hasattr(user, 'profile'):
-            context['is_bookmarked'] = Bookmark.objects.filter(
-                profile=user.profile, book=book
-            ).exists()
-
-            context['can_edit'] = (book.contributer == user.profile)
+        if self.request.user.is_authenticated and hasattr(self.request.user, 'profile'):
+            is_owner = book.contributor == self.request.user.profile
+        
+        context['is_owner'] = is_owner
 
         return context
 
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
 
-        if 'submit_review' in request.POST:
-            form = BookReviewForm(request.POST)
-            if form.is_valid():
-                review = form.save(commit=False)
-                review.book = self.object
-                if request.user.is_authenticated and hasattr(request.user, 'profile'):
-                    review.user_reviewer = request.user.profile
-                else:
-                    review.anon_reviewer = 'Anonymous'
-                review.save()
+        if 'bookmark' in request.POST:
+            return self.handle_bookmark(request)
 
-        elif 'toggle_bookmark' in request.POST:
-            if request.user.is_authenticated and hasattr(request.user, 'profile'):
-                profile = request.user.profile
-                bookmark = Bookmark.objects.filter(profile=profile, book=self.object)
-
-                if bookmark.exists():
-                    bookmark.delete()
-                else:
-                    Bookmark.objects.create(profile=profile, book=self.object)
+        if 'review' in request.POST:
+            return self.handle_review(request)
 
         return redirect(self.object.get_absolute_url())
+    
+    def handle_bookmark(self, request):
+
+        if request.user.is_authenticated and hasattr(request.user, 'profile'):
+
+            Bookmark.objects.get_or_create(
+                profile=request.user.profile,
+                book=self.object
+            )
+
+        return redirect(self.object.get_absolute_url())
+    
+    def handle_review(self, request):
+
+        form = BookReviewForm(request.POST)
+
+        if form.is_valid():
+            review = form.save(commit=False)
+
+            review.book = self.object
+
+            if request.user.is_authenticated and hasattr(request.user, 'profile'):
+                review.user_reviewer = request.user.profile
+            else:
+                review.anon_reviewer = 'Anonymous'
+
+            review.save()
+
+        return redirect(self.object.get_absolute_url())
+
+
+class BaseBorrowView(View):
+
+    def post(self, request, pk):
+
+        book = get_object_or_404(Book, pk=pk)
+
+        if not self.check_availability(book):
+            return redirect(self.get_redirect_url(book))
+
+        self.create_borrow(book, request)
+
+        return redirect(self.get_redirect_url(book))
+
+    def check_availability(self, book):
+        raise NotImplementedError
+
+    def create_borrow(self, book, request):
+        raise NotImplementedError
+
+    def get_redirect_url(self, book):
+        raise NotImplementedError
 
 
 class BookCreateView(LoginRequiredMixin, CreateView):
@@ -105,13 +151,12 @@ class BookCreateView(LoginRequiredMixin, CreateView):
     form_class = BookForm
     template_name = 'bookclub/book_form.html'
 
-    def dispatch(self, request, *args, **kwargs):
-        if not is_book_contributor(request.user):
-            return redirect('bookclub:book_list')
-        return super().dispatch(request, *args, **kwargs)
-
     def form_valid(self, form):
+        if not is_book_contributor(self.request.user):
+            return redirect('bookclub:book_list')
+
         form.instance.contributor = self.request.user.profile
+
         return super().form_valid(form)
 
 
@@ -119,72 +164,78 @@ class BookUpdateView(LoginRequiredMixin, UpdateView):
     model = Book
     form_class = BookForm
     template_name = 'bookclub/book_form.html'
+    context_object_name = 'book'
 
-    def dispatch(self, request, *args, **kwargs):
+    def get(self, request, *args, **kwargs):
+
         book = self.get_object()
 
-        if (
-            not is_book_contributor(request.user)
-            or book.contributor != request.user.profile
-        ):
+        if not is_book_contributor(request.user):
             return redirect('bookclub:book_list')
 
-        return super().dispatch(request, *args, **kwargs)
+        if book.contributor != request.user.profile:
+            return redirect('bookclub:book_detail', pk=book.pk)
+
+        return super().get(request, *args, **kwargs)
 
     def form_valid(self, form):
-        form.instance.contributor = self.get_object().contributor
+
+        book = self.get_object()
+
+        if not is_book_contributor(self.request.user):
+            return redirect('bookclub:book_list')
+
+        if book.contributor != self.request.user.profile:
+            return redirect('bookclub:book_detail', pk=book.pk)
+
         return super().form_valid(form)
 
 
-class BookBorrowView(View):
-
-
+class BookBorrowView(BaseBorrowView):
     def get(self, request, pk):
+
         book = get_object_or_404(Book, pk=pk)
 
-        if not book.available_to_borrow:
-            return redirect(book.get_absolute_url())
-
-        if request.user.is_authenticated and hasattr(request.user, "profile"):
+        if request.user.is_authenticated and hasattr(request.user, 'profile'):
             form = BorrowForm(initial={
-                "name": request.user.profile.display_name
+                'name': request.user.profile.display_name
             })
-            form.fields["name"].disabled = True
+
+            form.fields['name'].disabled = True
+
         else:
             form = BorrowForm()
 
-        return render(request, "bookclub/borrow_form.html", {
-            "form": form,
-            "book": book
+        return render(request, 'bookclub/borrow_form.html', {
+            'form': form,
+            'book': book
         })
 
+    def check_availability(self, book):
+        return book.available_to_borrow
 
-    def post(self, request, pk):
-        book = get_object_or_404(Book, pk=pk)
-
-        if not book.available_to_borrow:
-            return redirect(book.get_absolute_url())
+    def create_borrow(self, book, request):
 
         form = BorrowForm(request.POST)
 
         if form.is_valid():
+
             borrow = form.save(commit=False)
+
             borrow.book = book
 
-            if request.user.is_authenticated and hasattr(request.user, "profile"):
+            if request.user.is_authenticated and hasattr(request.user, 'profile'):
                 borrow.borrower = request.user.profile
                 borrow.name = request.user.profile.display_name
-            else:
-                borrow.name = form.cleaned_data["name"]
+
+            borrow.date_to_return = (
+                borrow.date_borrowed + timedelta(days=14)
+            )
 
             borrow.save()
 
             book.available_to_borrow = False
             book.save()
 
-            return redirect(book.get_absolute_url())
-
-        return render(request, "bookclub/borrow_form.html", {
-            "form": form,
-            "book": book
-        })
+    def get_redirect_url(self, book):
+        return book.get_absolute_url()
