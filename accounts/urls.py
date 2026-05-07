85b5edd0c9c23f272fe4cc4ee2766564efbbc5ce
jgfiguerres-at <justin.edward.figuerres@student.ateneo.edu>
2026-05-07 16:59:29 +0800

    Fix app-level & project-level URLs and registration templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+
 from .views import ProfileUpdateView
 
-url_patterns = [
-    path('<str:username>/', ProfileUpdateView.as_view(), name='profile-update'),
+urlpatterns = [
+    path('accounts/<str:username>/', ProfileUpdateView.as_view(), name='profile-update'),
 ]
 
 app_name = "accounts"
--- commongrounds/urls.py
+++ commongrounds/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('', include('bookclub.urls'), name='bookclub'),
     path('', include('diyprojects.urls'), name='diyprojects'),
     path('', include('commissions.urls'), name='commissions'),
-    path('accounts/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('accounts.urls'), name='accounts'),
+    path('', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
